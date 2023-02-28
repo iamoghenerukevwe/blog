bb7d883b6e4091f5e873fe9d2431764ea4b7b834
iamoghenerukevwe <50581464+iamoghenerukevwe@users.noreply.github.com>
2023-02-28 02:21:07 +0100

    UI

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import styles from '@/styles/header.module.css'
+
+export default function Header() {
+  return (
+    <section className={styles.header}>
+
+     <div className={styles.primary}>The difference between a flower and a weed is perspective.</div>
+     <div className={styles.subtext}>Hi i share my perspective on matters and  trending topics, technical writing and  everything that comes to  my  mind</div>
+
+    </section>
+  );
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import styles from '@/styles/Home.module.css'
 import {GraphQLClient, gql} from 'graphql-request';
 import BlogCard from 'components/BlogCard';
 import Nav from "../../components/Nav";
+import Header from "../../components/Header";
 
 const graph = new GraphQLClient('https://api-us-east-1.hygraph.com/v2/clbogk62k0d0x01uq8rcf36i8/master')
 
@@ -48,9 +49,12 @@ export default function Home({posts}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Nav/>
+      <Header/>
+
       <main className={styles.main}>
-      
-        <Nav />
+        
         
         {posts.map((post) => (
           <BlogCard
