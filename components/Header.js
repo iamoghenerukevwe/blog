47f719999d6e42466a4340b7c1127aa7c15b1e47
iamoghenerukevwe <50581464+iamoghenerukevwe@users.noreply.github.com>
2023-03-01 17:05:14 +0100

    color

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <section className={styles.header}>
 
-     <div className={styles.primary}>The difference between a flower and <br/>a weed is perspective.</div>
-     <div className={styles.subtext}>Here I share my perspective and assistance on tech topics, green energy, <br/>technical writing and everything in between</div>
+     <div className={styles.primary}>The difference between a flower and a weed is perspective.</div>
+     <div className={styles.subtext}>Here I share my perspective and assistance on tech topics, green energy, technical writing and everything in between.</div>
 
     </section>
   )
